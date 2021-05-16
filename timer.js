0fdc7d6e27b5c0d06b5a867fef81f1dd431f0930
evange_domi <pqln6430@gmail.com>
2021-05-16 17:06:04 +0900

    アカウント？

--- timer.js
+++ timer.js
@@ -33,12 +33,11 @@ audio[2].src="mp3/03_pickup.mp3";
 audio[3]=new Audio();
 audio[3].src="mp3/04_pickcheck.mp3";
 
-//var picktime=[40,40,35,30,25,25,20,20,15,10,10,5,5,5,5];
-var picktime=[40,40,35,30,25,25,20,20,15,10,10,5,5,5,0];
+var picktime=[40,40,35,30,25,25,20,20,15,10,10,5,5,5,5];
 var cnt=0;
 var npick=0;
 var ncheck=0;
-//var interval=5000;
+var interval=5000;
 
 function picktimer(npick)
 {
@@ -100,9 +99,8 @@ function slidesw()
       npick++;
 
       //intervalの時間は1ピックで200ミリ秒減る
-      var timer2=setTimeout("picktimer(picktime[npick])",5000);
-      //var timer2=setTimeout("picktimer(picktime[npick])",interval);
-      //interval=interval - 200;
+      var timer2=setTimeout("picktimer(picktime[npick])",interval);
+      interval=interval - 200;
     }
   }
 }
